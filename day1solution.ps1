b655f8b93978dd3578bb1958b696bf575e23b986
TVau <tyler.r.vaughan@gmail.com>
2023-11-30 22:08:46 -0800

    Day 1 cleanup

--- day1solution.ps1
+++ day1solution.ps1
@@ -1,16 +1,12 @@
 $text = Get-Content .\day1Input.ps1
 
-$numbers = "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
+$numbers = "\d", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"
 
 $regex = $numbers -join '|'
 
-$text | % {
-    $both = [regex]::Matches($_, "\d|$($regex)").Value
-    $first = [regex]::Match($_, "\d|$($regex)").Value
-    $last = [regex]::Match($_, "\d|$($regex)", [System.Text.RegularExpressions.RegexOptions]::RightToLeft).Value
-    # $first = $both[0]
-    # $last = $both[-1]
-    $output = [int](($first -as [int] ? $first : ($numbers.IndexOf($first) + 1).ToString()) + ($last -as [int] ? $last : ($numbers.IndexOf($last) + 1).ToString()))
-    Write-Host ("First: {0} Last: {1} Output: {2}" -f $first, $last, $output)
-    $output
+$text | Foreach-Object {
+    $first = [regex]::Match($_, $regex).Value
+    $last = [regex]::Match($_, $regex, [System.Text.RegularExpressions.RegexOptions]::RightToLeft).Value
+
+    [int](($first -as [int] ? $first : $numbers.IndexOf($first).ToString()) + ($last -as [int] ? $last : $numbers.IndexOf($last).ToString()))
 } | Measure-Object -Sum
